c405d6eb6f7930ae46a85a099fa6e00b8b7bc1ab
Furkyleon <furkan.yilmaz@sabanciuniv.edu>
2026-09-07 20:57:34 +0300

    chore: initialize Spring Boot backend and React frontend

---
+++ backend/src/main/java/com/aselsan/queuemonitor/QueueMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.aselsan.queuemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueMonitorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aselsan/queuemonitor/QueueMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aselsan.queuemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
